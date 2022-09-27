be47a777c8b75293907f664c2e4a6ddff98e16d3
grandsonneo@gmail.com <grandsonneo@gmail.com>
2022-09-27 19:30:11 -0400

    Update

--- lifesim.py
+++ lifesim.py
@@ -546,7 +546,7 @@ while True:
 				if not relation.was_complimented:
 					p.change_karma(randint(0, 3))
 					relation.change_relationship(round_stochastic(appreciation / 6))
-					if randint(1, 200) <= appreciation:
+					if randint(1, 300) <= round_stochastic(appreciation * relation.relationship/50):
 						display_event(_("Your {relation} complimented you back!").format(relation=relation.name_accusative()))
 						p.change_happiness(randint(5, 9))
 					relation.was_complimented = True
