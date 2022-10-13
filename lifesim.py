46df928ad3522a76097c096e945bd5b11eaf26af
grandsonneo@gmail.com <grandsonneo@gmail.com>
2022-10-13 12:15:31 -0400

    Commit

--- lifesim.py
+++ lifesim.py
@@ -14,13 +14,13 @@ TODO List:
 """
 
 while True:
-    clear_screen()
-    try:
-        player = start_menu()
-        print(_("Age {age}").format(age=player.age))
-        while True:
-            main_menu(player)
-            player.save_game()
-    except PlayerDied:
-        if not yes_no(_("Would you like to start a new life?")):
-            break
+	clear_screen()
+	try:
+		player = start_menu()
+		print(_("Age {age}").format(age=player.age))
+		while True:
+			main_menu(player)
+			player.save_game()
+	except PlayerDied:
+		if not yes_no(_("Would you like to start a new life?")):
+			break
