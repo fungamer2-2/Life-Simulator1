f675aa63ab29a6ceafbff17d68220fd64fc52ee9
fungamer2-2 <fungamer2-2@users.noreply.github.com>
2022-10-13 19:23:45 +0000

    :art: Format Python code with psf/black

--- lifesim-translate.py
+++ lifesim-translate.py
@@ -1,6 +1,6 @@
 import os
 
-#Extracts lifesim.py files for translation
+# Extracts lifesim.py files for translation
 
 print("Finding translations...")
 FOLDER = os.getcwd()
@@ -8,24 +8,24 @@ FOLDER = os.getcwd()
 skip_dirs = [".git", ".github", "__pycache__"]
 candidates = []
 for path, dirs, files in os.walk(FOLDER, topdown=True):
-	for d in skip_dirs:
-		if d in dirs:
-			dirs.remove(d)
-			#print("Skipping " + d)
-	for file in files:
-		if file.endswith(".py") and file != "pygettext.py":
-			candidates.append((path+ "/" + file).removeprefix(FOLDER + "/"))
+    for d in skip_dirs:
+        if d in dirs:
+            dirs.remove(d)
+            # print("Skipping " + d)
+    for file in files:
+        if file.endswith(".py") and file != "pygettext.py":
+            candidates.append((path + "/" + file).removeprefix(FOLDER + "/"))
 
 os.chdir(FOLDER)
 
 names = []
 for name in candidates:
-	f = open(FOLDER + "/" + name, "r")
-	contents = f.read()
-	if "_(\"" in contents or "_('":
-		names.append(name)
-		
-all = " ".join(map(lambda s: "\"" + s + "\"", names))
+    f = open(FOLDER + "/" + name, "r")
+    contents = f.read()
+    if '_("' in contents or "_('":
+        names.append(name)
+
+all = " ".join(map(lambda s: '"' + s + '"', names))
 print("Extracting strings...")
 
 os.system("python pygettext.py -d lifesim " + all)
